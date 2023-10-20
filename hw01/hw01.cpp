85d5ccc5e16aacaac97b0b3f9154c0071404068a
Jonas Jelten <jj@sft.lol>
2023-10-20 12:21:56 +0200

    add hw01

---
+++ hw01/hw01.cpp
@@ -0,0 +1,11 @@
+// main function to test your work locally
+
+#include <iostream>
+
+
+static_assert(__cplusplus >= 202002L);
+
+int main() {
+    int library_number = library_function();
+    std::cout << library_number << std::endl;
+}
---
+++ hw01/library.cpp
@@ -0,0 +1,7 @@
+/**
+ * Our precious library function, returning
+ * the answer to the Ultimate Question of Life, the Universe, and Everything.
+ */
+int library_function() {
+    return 1337;
+}
