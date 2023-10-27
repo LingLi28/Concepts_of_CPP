5f147f643a0cb91c35524aa88655b7befeda917a
Jonas Jelten <jj@sft.lol>
2023-10-27 12:53:00 +0200

    add hw02

---
+++ hw02/combinatorics.cpp
@@ -0,0 +1,19 @@
+#include "combinatorics.h"
+
+
+uint64_t factorial(uint64_t val) {
+    // TODO
+    return 0;
+}
+
+
+uint64_t permutation(uint64_t val, uint64_t val2) {
+    // TODO
+    return 0;
+}
+
+
+uint64_t combination(uint64_t val, uint64_t val2) {
+    // TODO
+    return 0;
+}
---
+++ hw02/run.cpp
@@ -0,0 +1,12 @@
+#include "combinatorics.h"
+#include "hw02.h"
+
+#include <iostream>
+
+
+int main() {
+    // here you can test your functions
+    std::cout << "perm(10, 3)=" << permutation(10, 3) << std::endl;
+
+    return 0;
+}
