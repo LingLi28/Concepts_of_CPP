fff6335e81c65982b053626840058921ba758d15
vboxuser <vboxuser@Linux.myguest.virtualbox.org>
2023-10-27 23:04:04 +0200

    hw02

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -3,17 +3,20 @@
 
 uint64_t factorial(uint64_t val) {
     // TODO
-    return 0;
+    if (val== 0) return 1;
+    return val * factorial(val - 1);
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    if (val < val2) return 0;
+    return factorial(val) / factorial(val - val2);
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    if (val < val2) return 0;
+    return factorial(val) / (factorial(val2) * factorial(val - val2));
 }
